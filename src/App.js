3c77cbf6c4eb2f617bbd201a2425a3dd62740f7d
2213607 <seobeen624@sookmyung.ac.kr>
2024-11-18 00:53:58 +0900

    폴더생성

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React, { Component } from "react";
-import "./App.css";
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1 className="title">두잇! 리액트 시작하기</h1>
-      </div>
-    );
-  }
+// src/App.js
+
+import React from "react";
+import Login from "./pages/Login";
+
+function App() {
+  return (
+    <div className="App">
+      <Login />
+    </div>
+  );
 }
+
 export default App;
