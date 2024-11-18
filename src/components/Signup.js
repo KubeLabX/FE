e16544758be0c345f8409f2ec0bd24c172885472
2213607 <seobeen624@sookmyung.ac.kr>
2024-11-18 21:11:44 +0900

    로그인/회원가입 완료

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,30 +1,96 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import "./Login.css";
+import TextField from "@mui/material/TextField";
+import Checkbox from "@mui/material/Checkbox";
+import Button from "@mui/material/Button";
+import FormControlLabel from "@mui/material/FormControlLabel";
+//import Link from "@mui/material/Link";
+import Typography from "@mui/material/Typography";
 
 function Signup() {
+  const [selectedStuPro, setSelectedOption] = useState("");
+
+  const handleChange = (event) => {
+    setSelectedOption(event.target.value);
+  };
   return (
-    <div>
-      <h2>회원가입</h2>
+    <div className="form-container">
+      <Typography component="h1" variant="h5">
+        SIGN UP
+      </Typography>
+
+      <div className="link-container">
+        <FormControlLabel
+          control={
+            <Checkbox
+              value="학생"
+              color="primary"
+              checked={selectedStuPro === "학생"}
+              onChange={handleChange}
+            />
+          }
+          label="학생"
+        />
+        <FormControlLabel
+          control={
+            <Checkbox
+              value="강사"
+              color="primary"
+              checked={selectedStuPro === "강사"}
+              onChange={handleChange}
+            />
+          }
+          label="강사"
+        />
+      </div>
+
       <form>
-        <label>
-          이름:
-          <input type="text" name="name" required />
-        </label>
+        <TextField
+          label="이름"
+          required
+          name="text"
+          autoFocus
+          className="input-field"
+        />
         <br />
-        <label>
-          이메일:
-          <input type="email" name="email" required />
-        </label>
+        <TextField
+          label="학번/교번"
+          required
+          name="text"
+          autoFocus
+          className="input-field"
+        />
         <br />
-        <label>
-          비밀번호:
-          <input type="password" name="password" required />
-        </label>
+        <TextField
+          label="비밀번호"
+          type="password"
+          required
+          name="password"
+          autoComplete="current-password"
+          className="input-field"
+        />
         <br />
-        <button type="submit">회원가입</button>
+        <TextField
+          label="비밀번호 확인"
+          type="password"
+          required
+          name="password"
+          autoComplete="current-password"
+          className="input-field"
+        />
+        <br />
+        <Button
+          type="submit"
+          variant="contained"
+          sx={{ marginTop: "16px", width: "300px" }}
+        >
+          회원 가입
+        </Button>
       </form>
+
       <p>
-        이미 회원이신가요? <Link to="/">로그인</Link>
+        이미 회원이신가요? <Link to="/">로그인하기</Link>
       </p>
     </div>
   );
