ec75c6e656149e4986dec43b87c8fa56c678d597
yeonjin yi <c2yjcs03@sookmyung.ac.kr>
2024-11-21 19:23:41 +0900

    Login, Signup css 변경

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -6,9 +6,68 @@ import TextField from "@mui/material/TextField";
 import Checkbox from "@mui/material/Checkbox";
 import Button from "@mui/material/Button";
 import FormControlLabel from "@mui/material/FormControlLabel";
+import styled from "styled-components";
 //import Link from "@mui/material/Link";
-
 import Typography from "@mui/material/Typography";
+
+const LoginContainer = styled.div`
+  background-color: #E6EEFF;
+  padding: 30px;
+  border-radius: 8px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: fit-content;
+  min-width: 400px;
+`;
+
+const InputField = {
+  '& .MuiOutlinedInput-root': {
+    borderRadius: '20px',
+    backgroundColor: 'rgba(75, 135, 255, 0.5)',
+    '& fieldset': {
+      borderColor: '#dde6ff',
+    },
+    '&:hover fieldset': {
+      borderColor: '#dde6ff',
+    },
+    '&.Mui-focused fieldset': {
+      borderColor: '#2563eb',
+    }
+  },
+  '& .MuiInputLabel-root': { // label 
+    color: '#FFFFFF',
+    fontWeight: 'bold',
+    '&.Mui-focused': {
+      color: '#000000',
+    }
+  },
+  width: '350px',
+  marginBottom: '10px'
+};
+
+const checkboxStyle = {
+  '&.Mui-checked': {
+    '& .MuiSvgIcon-root': {
+      color: '#00205F', // 체크 표시 색상 (진한 남색)
+    }
+  },
+  '&:hover': {
+    backgroundColor: 'transparent',
+  },
+  '& .MuiTouchRipple-root': { // ripple 효과 제거
+    display: 'none'
+  }
+};
+
+const labelStyle = {
+  color: '#00205F',
+  '& .MuiFormControlLabel-label': {
+    fontWeight: 'bold',
+    fontSize: '14px'
+  },
+  marginRight: '16px'
+};
 
 export default function App() {
   const [selectedStuPro, setSelectedOption] = useState("");
@@ -26,72 +85,79 @@ export default function App() {
 
   return (
     <div className="form-container">
-      <Typography component="h1" variant="h5"
-        sx={{
-          fontSize: '2rem',
-          fontWeight: 'bold'
-        }}>
-        LOG IN
-      </Typography>
-      <TextField
-        label="학번/교번"
-        required
-        name="email"
-        autoFocus
-        className="input-field"
-      />
-      <TextField
-        label="비밀번호"
-        type="password"
-        required
-        name="password"
-        autoComplete="current-password"
-        className="input-field"
-      />
-      <div className="link-container">
-        <div>
-          <Link href="#">비밀번호 찾기</Link>
-        </div>
-        <div>
-          <Link to="#" onClick={handleSignupClick}>
-            회원가입
-          </Link>
-        </div>
-      </div>
-
-      <div className="link-container">
-        <FormControlLabel
-          control={
-            <Checkbox
-              value="강사"
-              color="primary"
-              checked={selectedStuPro === "강사"}
-              onChange={handleChange}
-            />
-          }
-          label="강사인가요?"
+      <LoginContainer>
+        <Typography component="h1" variant="h5"
+          sx={{
+            fontSize: '2rem',
+            fontWeight: 'bold',
+            marginBottom: '30px',
+            color: '#00205F'
+          }}>
+          LOG IN
+        </Typography>
+        <TextField
+          label="학번/교번"
+          required
+          name="email"
+          autoFocus
+          className="input-field"
+          sx={InputField}
         />
-        <FormControlLabel
-          control={
-            <Checkbox
-              value="학생"
-              color="primary"
-              checked={selectedStuPro === "학생"}
-              onChange={handleChange}
-            />
-          }
-          label="학생인가요?"
+        <TextField
+          label="비밀번호"
+          type="password"
+          required
+          name="password"
+          autoComplete="current-password"
+          className="input-field"
+          sx={InputField}
         />
-      </div>
+        <div className="link-container">
+          <FormControlLabel
+            control={
+              <Checkbox
+                value="강사"
+                color="primary"
+                checked={selectedStuPro === "강사"}
+                onChange={handleChange}
+                sx={checkboxStyle}
+              />
+            }
+            label="강사인가요?"
+            sx={labelStyle}
+          />
+          <FormControlLabel
+            control={
+              <Checkbox
+                value="학생"
+                color="primary"
+                checked={selectedStuPro === "학생"}
+                onChange={handleChange}
+                sx={checkboxStyle}
+              />
+            }
+            label="학생인가요?"
+            sx={labelStyle}
+          />
+        </div>
 
-      <Button
-        type="submit"
-        variant="contained"
-        sx={{ marginTop: "16px", width: "300px" }}
-        onClick={() => navigate("/main")}
-      >
-        로그인
-      </Button>
+        <div className="link-container">
+          <div>
+            <Link to="#" onClick={handleSignupClick}>
+              회원가입 하러가기
+            </Link>
+          </div>
+        </div>
+
+        <Button
+          type="submit"
+          variant="contained"
+          className="loginbtn"
+          onClick={() => navigate("/main")}
+        >
+          로그인
+        </Button>
+      </LoginContainer>
     </div>
   );
 }
--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,12 +1,72 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import "../css/Login.css";
+import "../css/Signup.css";
 import TextField from "@mui/material/TextField";
 import Checkbox from "@mui/material/Checkbox";
 import Button from "@mui/material/Button";
 import FormControlLabel from "@mui/material/FormControlLabel";
 //import Link from "@mui/material/Link";
 import Typography from "@mui/material/Typography";
+import styled from "styled-components";
+
+const SignupContainer = styled.div`
+  background-color: #E6EEFF;
+  padding: 30px;
+  border-radius: 8px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: fit-content;
+  min-width: 400px;
+`;
+
+const InputField = {
+  '& .MuiOutlinedInput-root': {
+    borderRadius: '20px',
+    backgroundColor: 'rgba(75, 135, 255, 0.5)',
+    '& fieldset': {
+      borderColor: '#dde6ff',
+    },
+    '&:hover fieldset': {
+      borderColor: '#dde6ff',
+    },
+    '&.Mui-focused fieldset': {
+      borderColor: '#2563eb',
+    }
+  },
+  '& .MuiInputLabel-root': { // label 
+    color: '#FFFFFF',
+    fontWeight: 'bold',
+    '&.Mui-focused': {
+      color: '#000000',
+    }
+  },
+  width: '350px',
+  marginBottom: '15px'
+};
+
+const checkboxStyle = {
+  '&.Mui-checked': {
+    '& .MuiSvgIcon-root': {
+      color: '#00205F', // 체크 표시 색상 (진한 남색)
+    }
+  },
+  '&:hover': {
+    backgroundColor: 'transparent',
+  },
+  '& .MuiTouchRipple-root': { // ripple 효과 제거
+    display: 'none'
+  }
+};
+
+const labelStyle = {
+  color: '#00205F',
+  '& .MuiFormControlLabel-label': {
+    fontWeight: 'bold',
+    fontSize: '14px'
+  },
+  marginRight: '16px'
+};
 
 function Signup() {
   const navigate = useNavigate();
@@ -17,87 +77,98 @@ function Signup() {
   };
   return (
     <div className="form-container">
-      <Typography component="h1" variant="h5"
-        sx={{
-          fontSize: '2rem',
-          fontWeight: 'bold'
-        }}>
-        SIGN UP
-      </Typography>
+      <SignupContainer>
+        <Typography component="h1" variant="h5"
+          sx={{
+            fontSize: '2rem',
+            fontWeight: 'bold'
+          }}>
+          SIGN UP
+        </Typography>
 
-      <div className="link-container">
-        <FormControlLabel
-          control={
-            <Checkbox
-              value="학생"
-              color="primary"
-              checked={selectedStuPro === "학생"}
-              onChange={handleChange}
-            />
-          }
-          label="학생"
-        />
-        <FormControlLabel
-          control={
-            <Checkbox
-              value="강사"
-              color="primary"
-              checked={selectedStuPro === "강사"}
-              onChange={handleChange}
-            />
-          }
-          label="강사"
-        />
-      </div>
+        <div className="link-container">
+          <FormControlLabel
+            control={
+              <Checkbox
+                value="학생"
+                color="primary"
+                checked={selectedStuPro === "학생"}
+                onChange={handleChange}
+                sx={checkboxStyle}
+              />
+            }
+            label="학생"
+            sx={labelStyle}
+          />
+          <FormControlLabel
+            control={
+              <Checkbox
+                value="강사"
+                color="primary"
+                checked={selectedStuPro === "강사"}
+                onChange={handleChange}
+                sx={checkboxStyle}
+              />
+            }
+            label="강사"
+            sx={labelStyle}
+          />
+        </div>
 
-      <form>
-        <TextField
-          label="이름"
-          required
-          name="text"
-          autoFocus
-          className="input-field"
-        />
-        <br />
-        <TextField
-          label="학번/교번"
-          required
-          name="text"
-          autoFocus
-          className="input-field"
-        />
-        <br />
-        <TextField
-          label="비밀번호"
-          type="password"
-          required
-          name="password"
-          autoComplete="current-password"
-          className="input-field"
-        />
-        <br />
-        <TextField
-          label="비밀번호 확인"
-          type="password"
-          required
-          name="password"
-          autoComplete="current-password"
-          className="input-field"
-        />
-        <br />
-        <Button
-          type="submit"
-          variant="contained"
-          sx={{ marginTop: "16px", width: "300px" }}
-          onClick={() => navigate("/")}
-        >
-          회원 가입
-        </Button>
-      </form>
+        <form>
+          <TextField
+            label="이름"
+            required
+            name="text"
+            autoFocus
+            className="input-field"
+            sx={InputField}
+          />
+          <br />
+          <TextField
+            label="학번/교번"
+            required
+            name="text"
+            autoFocus
+            className="input-field"
+            sx={InputField}
+          />
+          <br />
+          <TextField
+            label="비밀번호"
+            type="password"
+            required
+            name="password"
+            autoComplete="current-password"
+            className="input-field"
+            sx={InputField}
+          />
+          <br />
+          <TextField
+            label="비밀번호 확인"
+            type="password"
+            required
+            name="password"
+            autoComplete="current-password"
+            className="input-field"
+            sx={InputField}
+          />
+          <br />
+          <Button
+            type="submit"
+            variant="contained"
+            sx={{ marginTop: "16px", width: "300px" }}
+            onClick={() => navigate("/")}
+            className="signupbtn"
+          >
+            회원 가입
+          </Button>
+        </form>
 
-      <p>
-        이미 회원이신가요? <Link to="/">로그인하기</Link>
-      </p>
+        <p>
+          이미 회원이신가요? <Link to="/">로그인하기</Link>
+        </p>
+      </SignupContainer>
     </div>
   );
 }
